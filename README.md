3d230092b119375d02e51072892cd22fba77d6cd
baran0119 <baran0119@gmail.com>
2016-08-11 00:26:44 +0200

    new one of vendor

---
+++ README.md
@@ -0,0 +1,4 @@
+android_vendor_samsung_grandprimeltexx
+==============================
+
+Proprietary Blobs for Samsung Galaxy Grand Prime [grandprimeltexx]
---
+++ copy_from_new.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+pathToSystemDir=~/android/G530FZXXU1BPF1_G530FZOXX1BPG1_XEO/system
+
+for file in $(find proprietary); do
+
+	if [ -f  ${file} ]; then
+		file2=$(echo ${file|sed 's:proprietary/::')
+
+		if [ -f ${pathToSystemDir}/${file2} ]; then
+			rm ${file}
+			cp ${pathToSystemDir}/${file2} ${file}
+			echo "Done : ${file2}"
+		else
+			echo -e "\t DO NOT EXIST: ${file2}"
+		fi
+
+	fi
+
+done
+
+#-e 	 DO NOT EXIST: lib/modules/msm-buspm-dev.ko
+#-e 	 DO NOT EXIST: bin/qmiproxy
